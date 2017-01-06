da173e17455a61c8d48f9267fa9a1856e7b5a9fd
smiroshnikov <sergeim.qa@gmail.com>
2017-01-06 19:57:35 +0200

    1+1

---
+++ src/test/java/MyMathTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyMathTest {
+    @Test
+    public void canAddOnePlusOne() {
+        int answer = 1 +31;
+        assertEquals("1+1=2", 2, answer);
+    }
+}
