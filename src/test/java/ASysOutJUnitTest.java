852ac4491c9cc458672b807f6c99bcfea8248eb7
smiroshnikov <sergeim.qa@gmail.com>
2017-01-07 20:51:09 +0200

    Test suite 1

--- src/test/java/ASysOutJUnitTest.java
+++ src/test/java/ASysOutJUnitTest.java
@@ -1,14 +1,23 @@
 import javafortesters.chapter3.practice.AClassWithAMethod;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ASysOutJUnitTest {
     @Test
     public void canItPrintToConsole(){
         AClassWithAMethod testObject = new AClassWithAMethod();
         testObject.aMethodOnACLass();
-
-
     }
 
-
+    @Test
+    public void canAddOnePlusOne() {
+        int answer = 1 +1;
+        assertEquals("FAILED Incorrect result  1+1=2", 2, answer);
+    }
+    @Test
+    public void compareStrings(){
+        String result = "Kikibojjo";
+        assertEquals("Error strings differ !","Kikibojo", result);
+    }
 }
