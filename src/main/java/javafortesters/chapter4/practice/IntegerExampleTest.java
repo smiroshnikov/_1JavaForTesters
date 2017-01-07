ee4191ff483bc9cfe886164e37c99fde9f4330c9
smiroshnikov <sergeim.qa@gmail.com>
2017-01-07 22:48:58 +0200

    Chapter4 classwork

---
+++ src/main/java/javafortesters/chapter4/practice/IntegerExampleTest.java
@@ -0,0 +1,17 @@
+package javafortesters.chapter4.practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IntegerExampleTest {
+    @Test
+    public void integerExploration() {
+        Integer four = new Integer(4);
+        assertEquals("intValue returns int 5", 4, four.intValue());
+        Integer five = new Integer("5");//accepts strings
+        assertEquals("Passed in String parameter ! ", 5, five.intValue());
+        Integer six = 6; // no need for "new" keyword
+        assertEquals("autoboxing assignment for 6 ", 6, six.intValue());
+    }
+}
