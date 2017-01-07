e3caf25fc996452578b52a2229d5bebc279bd335
smiroshnikov <sergeim.qa@gmail.com>
2017-01-07 12:13:04 +0200

    Chapter5 classwork

--- src/test/java/chapter5/TestAppEnvironmentTest.java
+++ src/test/java/chapter5/TestAppEnvironmentTest.java
@@ -2,11 +2,13 @@ package chapter5;
 
 import javafortesters.chapter5.practice.TestAppEnv;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestAppEnvironmentTest {
     @Test
     public void canGetUrlStatically() {
-        TestAppEnv.getUrl();
+        String result = "google.com";
+        assertEquals("URL does not match",TestAppEnv.getUrl(),result);
         // because its static method it can be used without instantiating TestAppEnv object
 
     }
