0bf75267f22a5309e95123c3c190f5ab371a74fa
smiroshnikov <sergeim.qa@gmail.com>
2017-01-14 23:34:15 +0200

    Asserts - before Hamcrest matchers

--- src/test/java/chapter14/Chapter14Classwork.java
+++ src/test/java/chapter14/Chapter14Classwork.java
@@ -2,11 +2,26 @@ package chapter14;
 
 import javafortesters.chapter13.practice.InvalidPassword;
 import javafortesters.chapter6.practice.User;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ExpectedException;
 
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+
+
 public class Chapter14Classwork {
+
+    @BeforeClass
+    public static void initialSetup() {
+        System.out.println("Beginning test suite SR-SP14");
+    }
+
+    @Before
+    public void nextTestPrompt() {
+        System.out.println("Now proceeding to the next test ! ");
+    }
+
     @Rule
     // expected exception declaration ?
     public ExpectedException expected = ExpectedException.none();
@@ -14,8 +29,30 @@ public class Chapter14Classwork {
     @Test
     public void invalidPasswordThrown() {
         expected.expect(InvalidPassword.class);
+        expected.expectMessage("Password must be 7 chars at least !");
+        // the line below is deprecated !
+        //expected.expectMessage(containsString("7 chars"));
         User user = new User("Myao", "123");
+    }
 
+    @Ignore("Feature is not yet supported in 63.21_01b")
+    @Test
+    public void thisTestIsIgnored() {
+    }
+
+    @Test
+    public void allAssertions() {
+        User user = new User();
+        assertEquals("username", user.getUsername());
+        assertFalse(user.getPassword().length() == 0);
+        assertTrue(Objects.equals(user.getPermission(), "Normal"));
+        assertNotSame(new User[]{user}, User.createUsers(1));
+
+    }
+
+    @AfterClass
+    public static void completionProcedure() {
+        System.out.println("Test Suite completed ! ");
     }
 
 }
