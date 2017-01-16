0a3a04b1f6977f70b8b1e8df60883d53f818aabc
smiroshnikov <sergeim.qa@gmail.com>
2017-01-16 18:59:36 +0200

    Chapter 15 - Solved classwork separated

--- src/main/java/playground1/MainRunner.java
+++ src/main/java/playground1/MainRunner.java
@@ -7,19 +7,20 @@ public class MainRunner {
     public static void main(String[] args) {
         String hello = "Hello fella!";
         List<Integer> result;
-        result = StringUtilityLetterCounter.returnNumberOfOccurrences(hello, "l");
+        result = StringUtilityHelpers.returnNumberOfOccurrences(hello, "l");
         System.out.printf("String was\" %s \" letter was \"l\"\n", hello);
-        for (int e :
-                result) {
-            System.out.println(e);
-        }
+        StringUtilityHelpers.printList(result);
         String huello = "Ahuello Zaeballo Razorvallo Evallo!";
         System.out.printf("String was \"%s \" letter was \"l\"\n", huello);
-        result = StringUtilityLetterCounter.returnNumberOfOccurrences(huello, "l");
-        for (int e :
-                result) {
-            System.out.println(e);
-        }
+        result = StringUtilityHelpers.returnNumberOfOccurrences(huello, "l");
+        StringUtilityHelpers.printList(result);
+        String zaeballo = "zzzzaeballi zveri zaebovivuvat1";
+        result = StringUtilityHelpers.returnNumberOfOccurrences(zaeballo, "z");
+        StringUtilityHelpers.printList(result);
+        System.out.printf("========================\n");
+        result = StringUtilityHelpers.returnNumberOfOccInReverse(zaeballo, "z");
+        StringUtilityHelpers.printList(result);
+
 
     }
 }
---
+++ src/main/java/playground1/StringUtilityHelpers.java
@@ -0,0 +1,35 @@
+package playground1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtilityHelpers {
+    public static List<Integer> returnNumberOfOccurrences(String full, String letter) {
+        List<Integer> occurrences = new ArrayList<>();
+        for (int i = 0; i < full.length(); i++) {
+            if (full.indexOf(letter, i) != -1) {
+                i = full.indexOf(letter, i);
+                occurrences.add(i);
+            }
+        }
+        return occurrences;
+    }
+
+    public static List<Integer> returnNumberOfOccInReverse(String full, String letter) {
+        List<Integer> occurrences = new ArrayList<>();
+        for (int i = full.length(); i >= 0; i--) {
+            if (full.lastIndexOf(letter, i) != -1) {
+                i = full.lastIndexOf(letter, i);
+                occurrences.add(i);
+            }
+        }
+        return occurrences;
+    }
+
+    public static void printList(List<Integer> whatever) {
+        for (int e :
+                whatever) {
+            System.out.println(e);
+        }
+    }
+}
