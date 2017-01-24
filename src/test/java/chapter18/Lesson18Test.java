f07ec677c89ba690dcbcbe260e4776b36abe0951
smiroshnikov <sergeim.qa@gmail.com>
2017-01-24 23:12:23 +0200

    Chapter18 - properties (p 267)

---
+++ src/test/java/chapter18/Lesson18Test.java
@@ -0,0 +1,52 @@
+package chapter18;
+
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
+
+public class Lesson18Test {
+    @Test
+    public void propertiesPush() {
+        Properties properties = new Properties();
+        assertThat(properties.size(), is(0));
+        properties.setProperty("browser", "firefox");
+        properties.setProperty("port", "8080");
+        assertThat(properties.getProperty("port"), is("8080"));
+        assertThat(properties.getProperty("golden_penis"), is(nullValue()));
+        for (String key :
+                properties.stringPropertyNames()) {
+            System.out.println("Key: " + key + " " + "Value: " + properties.getProperty(key));
+
+        }
+        // or better way
+        properties.list(System.out);
+    }
+
+    @Test
+    public void bobTest() {
+        Properties properties = new Properties();
+        properties.setProperty("name", "bob");
+        properties.setProperty("gender", "male");
+        properties.setProperty("passwords", "paSSw0rd");
+        assertThat(properties.size(), is(3));
+        properties.list(System.out);
+        assertThat(properties.getProperty("gender"), is(notNullValue()));
+        assertThat(properties.getProperty("permission", "Admin"), is("Admin"));
+        String workingDirectory = System.getProperty("user.dir");
+        System.out.println(workingDirectory);
+        // I can define a new path for resource files
+        // String resourceFilePath = workingDirectory +
+        // "/src/test/resources/" +
+        //"property_files/" +
+        //"static_example.properties";
+        Properties sys = System.getProperties();
+        sys.list(System.out);
+
+    }
+
+}
