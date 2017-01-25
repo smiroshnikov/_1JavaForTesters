ef52086e49797b4f2a671416c1921b4f4d8ecf88
smiroshnikov <sergeim.qa@gmail.com>
2017-01-25 21:48:40 +0200

    Chapter19 properties & files

--- src/test/java/chapter19/FilesTest.java
+++ src/test/java/chapter19/FilesTest.java
@@ -1,7 +1,42 @@
 package chapter19;
 
-/**
- * Created by Iidwuurliik on 1/25/2017.
- */
+import org.junit.Test;
+
+import java.io.*;
+
 public class FilesTest {
+
+    private static File filesLesson1() throws IOException {
+        //Valid
+        //File file = new File(System.getProperties().getProperty("user.home"));
+
+        File outputfile = File.createTempFile("myFile", ".txt");
+        PrintWriter print = new PrintWriter(new BufferedWriter(new FileWriter(outputfile)));
+        // What is this ?! Whats it is needed for ?
+
+        for (int lineNumber = 0; lineNumber < 6; lineNumber++) {
+            print.println("line" + lineNumber);
+        }
+        print.close();
+        return outputfile;
+    }
+
+    @Test
+    public void outputFileToSystemOutWithBufferedReader() throws IOException {
+        File inputFile = filesLesson1();
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                System.out.println(line);
+                System.out.println(line);
+            }
+        } finally {
+            reader.close();
+        }
+
+    }
+
 }
