170d25f33ceea296bcae2ad5087b43e724531620
smiroshnikov <zer00aeon@gmail.com>
2017-01-28 15:31:27 +0200

    Chapter 19 - first Spy :)

--- src/test/java/chapter19/DirectoryTests.java
+++ src/test/java/chapter19/DirectoryTests.java
@@ -4,6 +4,9 @@ package chapter19;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.StringTokenizer;
 
 public class DirectoryTests {
 
@@ -16,25 +19,38 @@ public class DirectoryTests {
 
         File[] fileList = tempDir.listFiles();
         String[] stringFileList = tempDir.list();
-        if (stringFileList != null) {
-            for (String s : stringFileList) {
-                System.out.println(s);
-            }
-        }
+//        if (stringFileList != null) {
+//            for (String s : stringFileList) {
+//                System.out.println(s);
+//            }
+//        }
         System.out.println("===========================same idea with filelist======================".toUpperCase());
         if (fileList != null) {
             for (File f :
                     fileList) {
-                if (f.isDirectory()){
-                    System.out.println("DIR->" + f.toString());
+                if (f.isDirectory()) {
+                    System.out.println("DR->" + f.toString());
+                    // TODO change date format, trim GMT and keep year , maybe Calendar or Stringbuilder
+                    System.out.printf("X [%b] , RD[%b], WR[%b], last modified [%s] %n",
+                            f.canExecute(), f.canRead(), f.canWrite(), (String.valueOf(new Date(f.lastModified()))));
                 }
-                System.out.println("FIL->" + f.toString());
+                System.out.println("FL->" + f.toString());
+                System.out.printf("X [%b] , RD[%b], WR[%b], last modified [%s] %n",
+                        f.canExecute(), f.canRead(), f.canWrite(), (String.valueOf(new Date(f.lastModified()))));
             }
         }
     }
 
     @Test
-    public void permissionsExample() {
+    public void permissionsExample() throws IOException {
+        File littleSpy = new File(System.getProperty("java.io.tmpdir"), "spy.bat");
+        System.out.println(littleSpy.getAbsolutePath());
+        System.out.println("File created " + littleSpy.createNewFile());
+        String fileContent = "\t\n" +
+                "@echo off\n" +
+                "echo Hello world.\n" +
+                "pause";
+
 
 
     }
