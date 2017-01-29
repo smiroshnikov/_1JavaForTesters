25281e50c5b1ad9ca181490d89923cc30f229087
smiroshnikov <sergeim.qa@gmail.com>
2017-01-29 23:25:26 +0200

    Chapter 20  - SortedSet love it

--- src/test/java/chapter21/CollectionRevisitedTest.java
+++ src/test/java/chapter21/CollectionRevisitedTest.java
@@ -27,10 +27,31 @@ public class CollectionRevisitedTest {
         alphaset.add("3");
         assertThat(alphaset.last(), is("z"));
         String[] alphas = new String[alphaset.size()];
+        // TODO useful conversion !
         alphaset.toArray(alphas);
         for (String e :
                 alphas) {
             System.out.println(e);
         }
+        System.out.println("==================================Headset GOES HERE=======================================\n");
+        SortedSet<String> subset = alphaset.headSet("b"); // returns strictly less than "b"
+        for (String e :
+            //subset) {
+                alphaset.headSet("b")) { // I like it more like this
+            System.out.println(e);
+        }
+        // tailset
+        System.out.println("==================================Tailset GOES HERE=======================================\n");
+        for (String e :
+                alphaset.tailSet("b")) { // includes b
+            System.out.println(e);
+        }
+        System.out.println("==================================Subset GOES HERE=======================================\n");
+        for (String e :
+                alphaset.subSet("3", "f")) { //"f" not included
+            System.out.println(e);
+        }
     }
+
+
 }
