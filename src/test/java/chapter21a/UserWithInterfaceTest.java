75e1a5ce6d794f7bb08c18693638c95ed8d6616f
smiroshnikov <sergeim.qa@gmail.com>
2017-01-30 22:50:59 +0200

    Chapter 21 - Sorted Map

--- src/test/java/chapter21a/UserWithInterfaceTest.java
+++ src/test/java/chapter21a/UserWithInterfaceTest.java
@@ -3,16 +3,13 @@ package chapter21a;
 import javafortesters.chapter21a.practice.User;
 import org.junit.Test;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by Iidwuurliik on 1/30/2017.
- */
+
 public class UserWithInterfaceTest {
     @Test
     public void sortedSetWithUserWithoutDup() {
@@ -36,6 +33,28 @@ public class UserWithInterfaceTest {
         }
         assertThat(bob.getUsername(), is(users[0].getUsername()));
         assertEquals(bob.getUsername(), users[0].getUsername());
+
+
+    }
+
+    @Test
+    public void sortedMapExample() {
+        SortedMap<String, String> map = new TreeMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+        map.put("key4", "value4");
+        map.put("key5", "value5");
+        map.put("key6", "value6");
+        map.put("key7", "value7");
+        assertThat(map.firstKey(), is("key1"));
+        assertThat(map.lastKey(), is("key7"));
+        SortedMap<String, String> headmap;
+        headmap = map.headMap("sdf");
+        for (Map.Entry<String, String> e :
+                map.entrySet()) {
+            System.out.printf("key -> %s , %s <--value  %n", e.getKey(), e.getValue());
+        }
 
 
     }
