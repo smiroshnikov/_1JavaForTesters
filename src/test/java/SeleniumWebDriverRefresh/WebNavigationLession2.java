7d4a768a260b0f59d2acaf2591cc4ad9b7968fcc
smiroshnikov <sergeim.qa@gmail.com>
2017-02-02 23:34:42 +0200

    Selenium Web Driver Studies - Provided Static Method for Chrome

--- src/test/java/SeleniumWebDriverRefresh/WebNavigationLession2.java
+++ src/test/java/SeleniumWebDriverRefresh/WebNavigationLession2.java
@@ -1,9 +1,13 @@
 package SeleniumWebDriverRefresh;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 
 public class WebNavigationLession2 {
@@ -18,6 +22,13 @@ public class WebNavigationLession2 {
     @Test
     public void navigateWithNAvigateTo() {
         driver.navigate().to("http://linux.org");
+        assertThat(driver.getTitle().contains("Linux"), is(true));
+    }
+
+    @AfterClass
+    // TODO  this is important because of the test fails , the driver would still quit!
+    public static void quitSDriver() {
+        driver.quit();
     }
 
 }
