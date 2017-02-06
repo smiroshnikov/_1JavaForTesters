ce173055483545f323dc0cd169d5a3a0aa628e90
smiroshnikov <sergeim.qa@gmail.com>
2017-02-06 12:37:23 +0200

    Selenium documentation revised

--- src/test/java/SeleniumWebDriverRefresh/part0/D/minus4/OpenBrowsersToGoogleTest.java
+++ src/test/java/SeleniumWebDriverRefresh/part0/D/minus4/OpenBrowsersToGoogleTest.java
@@ -2,6 +2,8 @@ package SeleniumWebDriverRefresh.part0.D.minus4;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -9,6 +11,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.io.FileNotFoundException;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertTrue;
 
@@ -18,6 +21,7 @@ public class OpenBrowsersToGoogleTest {
     private static final String CHROME_DRIVER_PATH = "C:\\webdrivers\\chromedriver.exe";
     private static final String EDGE_DRIVER_PATH = "C:\\webdrivers\\MicrosoftWebDriver.exe";
     private static final String FIREFOX_DRIVER_PATH = "C:\\webdrivers\\geckodriver.exe";
+    private By headerPageText = By.cssSelector(".hidden-small"); // important
 
     @Before
     public void chromeDriverPathSetup() throws FileNotFoundException {
@@ -32,15 +36,21 @@ public class OpenBrowsersToGoogleTest {
         if (System.getProperty("webdriver.gecko.driver") == null) {
             System.setProperty("webdriver.gecko.driver", FIREFOX_DRIVER_PATH);
         }
+        // debug had connection/permission issues with VM
         //System.out.println(System.getProperty("webdriver.chrome.driver"));
     }
 
     @Test
-    public void startChromeWebdriver() {
+    public void startChromeWebdriver() throws InterruptedException {
         WebDriver chomeDriver = new ChromeDriver();
         chomeDriver.navigate().to("http://www.google.com");
+        chomeDriver.manage().window().maximize();
         assertTrue("title should start with \"Google\" ", chomeDriver.getTitle().startsWith("Google"));
-        chomeDriver.close();
+        chomeDriver.findElement(By.name("q")).sendKeys("selenium", Keys.ENTER);
+        chomeDriver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        assertTrue(chomeDriver.findElement(By.cssSelector(".srg .g:n th-child(1)")).getText().contains("Selenuim"));
+        chomeDriver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        chomeDriver.close(); // use AfterClass
     }
 
     //    @Ignore
