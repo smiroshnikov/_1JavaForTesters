73ee5bb6453b2d3455b35c9713f51b22176eed2a
smiroshnikov <sergeim.qa@gmail.com>
2017-02-08 22:23:56 +0200

    ToDo MVC , completing tasks as intended

--- src/test/java/SeleniumWebDriverRefresh/Part3/D/minus1/TodOMVCTest.java
+++ src/test/java/SeleniumWebDriverRefresh/Part3/D/minus1/TodOMVCTest.java
@@ -11,6 +11,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 
 /**
  * todoMVC proof of concept
@@ -34,39 +37,79 @@ public class TodOMVCTest {
             System.setProperty("webdriver.edge.driver", EDGE_DRIVER_PATH);
         }
         driver = new ChromeDriver();
+        driver.manage().deleteAllCookies();
     }
 
     @AfterClass
     public static void closeDriver() {
-        driver.close();
+        //driver.close();
     }
 
     @Test
-    public void createTask() throws InterruptedException {
+    public void createTasks() {
         //Configuration.browser = "chrome";
         //open("http://todomvc.com/examples/troopjs_require/");
-        // apparently when i was missing "#" in link , my flow was fucked up . Its only 00-05 glad it took only aboout a fucking hour !
+        // apparently when i was missing "#" in link , my flow was fucked up . Its only 00-05 glad it took only aboout an hour !
         // sendKeys(Keys.Enter) was completely ignored
         driver.get("http://todomvc.com/examples/react/#/");
         driver.manage().window().maximize();
+        // different slector , just test my css skills :)
         //WebElement newTodo = driver.findElement(By.className("new-todo"));
         WebElement newTodo = driver.findElement(By.cssSelector(".new-todo"));
-        newTodo.sendKeys("write a todo list ");
+        newTodo.sendKeys("Write a todo list");
         newTodo.sendKeys(Keys.RETURN);
-        newTodo.sendKeys("go to sleep");
+        newTodo.sendKeys("Go to sleep, and sleep 6 hours at least");
         newTodo.sendKeys(Keys.RETURN);
-        newTodo.sendKeys("get a job offer :)");
+        newTodo.sendKeys("get a Java job offer :)");
         newTodo.sendKeys(Keys.RETURN);
-        newTodo.sendKeys("forget DELL-EMC as my worst fucking nightmare ");
+        newTodo.sendKeys("Improve my Java and Selenium skills");
         newTodo.sendKeys(Keys.RETURN);
 
         List<WebElement> tasks = driver.findElements(By.cssSelector(".todo-list li label"));
         // finally
         for (WebElement task :
                 tasks) {
-            System.out.printf(task.getText());
+            System.out.printf("task text : %s %n", task.getText());
         }
-        Thread.sleep(1000); // replace
+//        Thread.sleep(1000); // replace
+        assertThat("Expecting a first task text ", tasks.get(0).getText(), is("Write a todo list"));
+        assertThat("expecting total of 4 tasks", tasks.size(), is(4));
+//        WebElement deleteButton = driver.findElement(By.cssSelector(".toggle"));
+//        deleteButton.click();
+
+        List<WebElement> completeButtons = driver.findElements(By.cssSelector(".toggle"));
+        // works
+        //deleteButtons.get(2).click();
+        for (WebElement taskComletion :
+                completeButtons) {
+            taskComletion.click();
+        }
+
+    }
+
+    @Test
+    public void completeTaskSingleElement() {
+        // works !
+        WebElement deleteButton = driver.findElement(By.cssSelector(".toggle"));
+        deleteButton.click();
+    }
+
+    @Test
+    public void completeTasksFromWebElementList() {
+        List<WebElement> deleteButtons = driver.findElements(By.className(".toggle"));
+        deleteButtons.get(2).click();
+    }
+
+
+    @Test
+    public void deleteTask() {
+    }
+
+
+    @Test
+    public void editExistingTask() {
+        // too hard
+        List<WebElement> tasks = driver.findElements(By.cssSelector(".todo-list li label"));
     }
 
 }
