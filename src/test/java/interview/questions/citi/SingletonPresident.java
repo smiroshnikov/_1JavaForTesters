6e24d82238fd37fcbe93c513ad32b0ad05f11d6a
smiroshnikov <sergeim.qa@gmail.com>
2017-02-10 16:28:28 +0200

    CITI - first interview question , solution posted

---
+++ src/test/java/interview/questions/citi/SingletonPresident.java
@@ -0,0 +1,24 @@
+package interview.questions.citi;
+
+/**
+ * Q: desing and implement a singleton pattern
+ * example of usage - instantiate a web driver as a singleton
+ * Private constructor to restrict instantiation of the class from other classes.
+ * Private static variable of the same class that is the only instance of the class.
+ * Public static method that returns the instance of the class, this is the global
+ * access point for outer world to get the instance of the singleton class.
+ */
+public class SingletonPresident {
+
+    private static SingletonPresident instance;
+
+    private SingletonPresident() {
+    }
+
+    public static SingletonPresident getInstance() {
+        if (instance == null) {
+            instance = new SingletonPresident();
+        }
+        return instance;
+    }
+}
---
+++ src/test/java/interview/questions/citi/TestUniquenessOfSingleton.java
@@ -0,0 +1,19 @@
+package interview.questions.citi;
+
+/**
+ * Testing , the output show SAME objct instance
+ */
+public class TestUniquenessOfSingleton {
+    public static void main(String[] args) {
+
+        SingletonPresident iidwuurliikTheFirstAndOnly = SingletonPresident.getInstance();
+        System.out.println(iidwuurliikTheFirstAndOnly.toString());
+        SingletonPresident trump;
+        trump = SingletonPresident.getInstance();
+        System.out.println(trump.toString());
+        SingletonPresident hillary = SingletonPresident.getInstance();
+        System.out.println(hillary.toString());
+
+
+    }
+}
