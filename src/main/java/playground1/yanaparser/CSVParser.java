73e64c0950e86bf239002d204fd499be551f51d9
smiroshnikov <sergeim.qa@gmail.com>
2017-02-17 00:18:08 +0200

    CSV separator for Yana , base logic done

--- src/main/java/playground1/yanaparser/CSVParser.java
+++ src/main/java/playground1/yanaparser/CSVParser.java
@@ -3,26 +3,27 @@ package playground1.yanaparser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CSVParser {
     public static void main(String[] args) throws IOException {
-
         String csvFile = "C:\\webdrivers\\csv\\country.csv";
-
-        String line = "";
+        String line;
         String csvSeparator = ",";
+        List<String> l = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
-                String[] country = line.split(csvSeparator);
-                if (line.split(csvSeparator)[2].contains("16778240")) {
-                    System.out.printf("ooooooooooooo");
+
+                if (line.split(csvSeparator)[2].contains("16778240") || ((line.split(csvSeparator)[2].contains("16785408")))) {
+                    //System.out.printf("ooooooooooooo");
+                    l.add(line.split(csvSeparator)[2]);
                 }
-//                    System.out.printf("%s%n", country[2]);
-//                if (Objects.equals(country[2], "16778240")) {
-//                    System.out.printf("HEYHEYHEY!!!!!");
-//                    // TODO here I will create another array
-//                }
             }
+        }
+        for (String value :
+                l) {
+            System.out.println(value);
         }
     }
 }
