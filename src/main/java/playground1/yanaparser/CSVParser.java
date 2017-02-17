cfb8159be6d0ed127d1b96af7402468f5f86fab2
smiroshnikov <sergeim.qa@gmail.com>
2017-02-17 19:49:29 +0200

    CSV separator for Yana . Banging head on the wall

--- src/main/java/playground1/yanaparser/CSVParser.java
+++ src/main/java/playground1/yanaparser/CSVParser.java
@@ -4,14 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+//int offsetp[0.3] line from offset
 //[2,6,10,14]
+
 
 public class CSVParser {
     public static void main(String[] args) throws IOException {
-        String csvFile = "C:\\webdrivers\\csv\\mos2_vertical_30sec_3acc_green laser ulf_600 greating_10per_mapping_23hr_121216.txt";
+        String csvFile = "C:\\webdrivers\\csv\\country.csv";
         String line;
         int lineCounter = -1;
         String csvSeparator = " ";
@@ -19,19 +20,21 @@ public class CSVParser {
         List<String[]> importantValuesList = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
+                System.out.println(lineCounter + "!");
                 lineCounter += 1;
                 if (lineCounter == 0) {
-                    System.out.println("Do nothing...");
+                    System.out.println("Ingnoring header...");
                 } else {
                     importantValuesList.add(line.split(csvSeparator));
-
                 }
             }
         }
-        for (String[] longLine :
-                importantValuesList) {
-            System.out.println(Arrays.toString(longLine));
-        }
+
+//        for (String[] longLine :
+//                importantValuesList) {
+//            System.out.println(Arrays.toString(longLine));
+//        }
+        System.out.println(importantValuesList.size());
+        System.out.println(importantValuesList.get(1)[0]);
     }
 }
-
