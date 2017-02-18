d98cbbd7870c0f4b5e735969e258c2647ad9c526
smiroshnikov <sergeim.qa@gmail.com>
2017-02-19 00:45:41 +0200

    CSV separator for Yana . Created a method for parsing , added sample and mock files to git    TODO output to files

--- src/main/java/playground1/yanaparser/CSVParser.java
+++ src/main/java/playground1/yanaparser/CSVParser.java
@@ -7,7 +7,7 @@ package playground1.yanaparser;
  */
 
 // LESSONS LEARNED - use a small fucking file for testing with simple easily read predefined values !
-//TODO  DONT USE " " - no idea why , use "\\s+" -  research online later !!!
+// DONT USE " " - no idea why , use "\\s+" -  research online later !!!
 // ALWAYS , сцуко !  ALWAYS , use paper and pen and write down how you will implement things - this could have saved time
 // Refactor as soon as you are sure what the required snippet of code does !
 
@@ -16,14 +16,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
-//int offsetp[0.3] line from offset
-//[2,6,10,14]
-
-
 public class CSVParser {
     public static final File MAC_FILE_PATH = new File("/Users/sergei.miroshnikov/Downloads/testYana.csv");
     public static final File WIN_FILE_PATH = new File("C:\\Webdrivers\\csv\\cleanMockup.txt");
+    public static final List<String[]> FIRST_LASER = new ArrayList<>();
+    public static final List<String[]> SECOND_LASER = new ArrayList<>();
+    public static final List<String[]> THIRD_LASER = new ArrayList<>();
+    public static final List<String[]> FORTH_LASER = new ArrayList<>();
 
     /**
      * Removes millisecond timestamp from laser measurment line
@@ -31,7 +30,7 @@ public class CSVParser {
      * @param dirtyLine - line with timestamp
      * @return String[] with values only
      */
-    public static String[] cleanLineWithValues(String[] dirtyLine) {
+    public static String[] removeTimestampFromLine(String[] dirtyLine) {
         return Arrays.copyOfRange(dirtyLine, 1, dirtyLine.length);
     }
 
@@ -41,7 +40,8 @@ public class CSVParser {
      * @param laserMeasurmentsFile - a file from the laser microscope
      * @return List of String [] - each array represents a line with values from file
      */
-    public static List<String[]> createAlistFromFile(File laserMeasurmentsFile) {
+    public static List<String[]> createAlistFromFileAndRemoveAxisX(File laserMeasurmentsFile) {
+        // TODO separate AXIS removal (and saving it aside) in separate method
         String lineInFile;
         int lineCounter = -1;
         String csvSeparator = "\\s+"; // DONT  EVER FUCKING SPLIT BY " " - apparently this is not concidered as space in every OS !
@@ -50,7 +50,7 @@ public class CSVParser {
             while ((lineInFile = br.readLine()) != null) {
                 lineCounter += 1;
                 if (lineCounter == 0) {
-                    System.out.println("Header ignored when coverted file to list...");
+                    System.out.println("DEBUG :: INFO :: Wave Length AXIS ignored when coverted file to list...");
                 } else {
                     valuesList.add(lineInFile.split(csvSeparator));
                 }
@@ -66,13 +66,13 @@ public class CSVParser {
      *
      * @param list
      */
-    public static void printListWitharrays(List<String[]> list) {
-        for (String[] lineWithExcessiveValuesInarray :
+    public static void printMeasurementsList(List<String[]> list) {
+        for (String[] lineWithMillisecondTimestamp :
                 list) {
             System.out.printf("%n");
-            //System.out.println(lineWithExcessiveValuesInarray.length);
+            //System.out.println(lineWithMillisecondTimestamp.length);
             for (String singleMeasurement :
-                    lineWithExcessiveValuesInarray) {
+                    lineWithMillisecondTimestamp) {
                 System.out.print(singleMeasurement + " ");
             }
         }
@@ -86,46 +86,43 @@ public class CSVParser {
      * @param corruptedMeasurements - list with timestamp in milliseconds
      * @return
      */
-    public static List<String[]> extractCleanMeasurements(List<String[]> corruptedMeasurements) {
+    public static List<String[]> aggregateCleanDataToList(List<String[]> corruptedMeasurements) {
         List<String[]> cleanMeasurements = new ArrayList<>();
         for (String[] corruptedLine :
                 corruptedMeasurements) {
-            cleanMeasurements.add(cleanLineWithValues(corruptedLine));
+            cleanMeasurements.add(removeTimestampFromLine(corruptedLine));
         }
         return cleanMeasurements;
     }
 
-    public static void main(String[] args) throws IOException {
-        List<String[]> corruptedMeasurements = createAlistFromFile(WIN_FILE_PATH);
-        List<String[]> cleanMeasurements = extractCleanMeasurements(corruptedMeasurements);
-        List<String[]> firstLaser = new ArrayList<>();
-        List<String[]> secondLaser = new ArrayList<>();
-        List<String[]> thirdLaser = new ArrayList<>();
-        List<String[]> forthLaser = new ArrayList<>();
-
-        // TODO separate as method
+    public static void parseResultsPerLaser(List<String[]> fullMeasurementsList) {
         int lineCounter = 0;
-
         for (String[] cleanLine :
-                cleanMeasurements) {
+                fullMeasurementsList) {
             if ((lineCounter % 4 == 0)) {
-                firstLaser.add(cleanLine);
+                FIRST_LASER.add(cleanLine);
             }
             if (lineCounter % 4 == 1) {
-                secondLaser.add(cleanLine);
+                SECOND_LASER.add(cleanLine);
             }
             if (lineCounter % 4 == 2) {
-                thirdLaser.add(cleanLine);
+                THIRD_LASER.add(cleanLine);
             }
             if (lineCounter % 4 == 3) {
-                forthLaser.add(cleanLine);
+                FORTH_LASER.add(cleanLine);
             }
             lineCounter += 1;
         }
+    }
 
-
-        printListWitharrays(secondLaser);
-
-
+    public static void main(String[] args) throws IOException {
+        List<String[]> corruptedMeasurements = createAlistFromFileAndRemoveAxisX(WIN_FILE_PATH);
+        List<String[]> cleanMeasurements = aggregateCleanDataToList(corruptedMeasurements);
+        parseResultsPerLaser(cleanMeasurements);
+        printMeasurementsList(FIRST_LASER);
+        printMeasurementsList(SECOND_LASER);
+        printMeasurementsList(THIRD_LASER);
+        printMeasurementsList(FORTH_LASER);
+        // TODO file output to 4 separate files
     }
 }
