972f6ffa8ac5c06d81e42e333c381b6d6ed42de5
smiroshnikov <sergeim.qa@gmail.com>
2017-02-28 23:39:52 +0200

    CSS

--- src/test/java/Selenium_WebDriver_ARichardson/interrogation/FightingCss.java
+++ src/test/java/Selenium_WebDriver_ARichardson/interrogation/FightingCss.java
@@ -1,5 +1,21 @@
 package Selenium_WebDriver_ARichardson.interrogation;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+
 public class FightingCss {
+    @Test
+    public void cssTest() {
+        By pParagraphSelector = By.cssSelector("p[class='normal']");
+        By divSelector = By.cssSelector("div[class='specialDiv']");
+        By partialDivSelector = By.cssSelector("div[class*='Di']");
+        By partialDivSelectorAtTheEnd = By.cssSelector("div[class$='Di']");
+        By partialDivSelectorAtTheMiddle = By.cssSelector("div[class$='Di']");
+        By partialDivSelectorAtTheStart = By.cssSelector("div[class^='spe']");
+        By anywhereClassHasDefinedString = By.cssSelector("div[class~='has']");
+
+
+    }
 
 }
+
