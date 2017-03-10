e4bd9a73969669f420bfaef45afeb59fbcb2d9ac
smiroshnikov <sergeim.qa@gmail.com>
2017-03-10 14:59:42 +0200

    Java Generics lesson 4 dicecting Generics - how not to write bad code , Java 5 legacy horror

--- src/main/java/hexlet/generics/module1/Main.java
+++ src/main/java/hexlet/generics/module1/Main.java
@@ -17,12 +17,28 @@ public class Main {
         list.add(1);
         show(list); // show reprecents an old legacy method (Java 5) before Generics
         List fuckedUpList = new ArrayList(); // allowed in order to support Java 5 and below legacy code
-        fuckedUpList.add(1);
-        fuckedUpList.add(2);
-        fuckedUpList.add(3);
-        fuckedUpList.add("heloo!!!"); // no problem detected so far by ide , lets compile and run
+//        fuckedUpList.add(1);
+//        fuckedUpList.add(2);
+//        fuckedUpList.add(3);
+//        fuckedUpList.add("heloo!!!"); // no problem detected so far by ide , lets compile and run
         // WORKS !
         show(fuckedUpList);// this might work but will lead to unpredictable errors
+
+        // lets break stuff
+        Box<Integer> b1 = new Box<>(1);
+        Box b2 = b1; // b2 has no idea what b1 is , just reference
+        b2.setItem("Hello"); // will compile !
+
+        //box.setItem("What?!?1"); - will not compile
+        System.out.println(b2.getItem()); // how about that - look at the byte code and see that its all about Object !
+        // lets get more crazy than that and try to see what getItem of b1 wuill return
+        System.out.println(b1.getItem()); // OMG ! works because println accepts object as an input
+        Integer j = b1.getItem(); // runtime exception , when forced to cast to Integer and has a String "hello" so ClassCastException occurs
+        // brain is not melting but the idea in this tut is how NOT to write BAD code
+
+
+
+
 
     }
 
