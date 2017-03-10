e785aa7229c2c0b67bb1f52b9159aa28b85bba2d
smiroshnikov <sergeim.qa@gmail.com>
2017-03-10 13:07:46 +0200

    Java Generics lesson 3 , multi typed generic class , inheritance

--- src/main/java/hexlet/Main.java
+++ src/main/java/hexlet/Main.java
@@ -3,18 +3,27 @@ package hexlet;
 public class Main {
     public static void main(String[] args) {
         final Tuple<String> stringTuple = new Tuple<>("left", "right");
-        System.out.println(stringTuple.getLeft());
+//        System.out.println(stringTuple.getLeft());
         final Tuple<Integer> integerTuple = new Tuple<>(1, 234);
-        System.out.println(integerTuple.getLeft() + 25);
+//        System.out.println(integerTuple.getLeft() + 25);
         // here we can see the whole beauty of generics , I had no idea how my generic tuple will be used
         // or with what classes it will be instantiated
         final Tuple<SuperClass> superClassTuple = new Tuple<>(new SuperClass(), new SuperClass());
-        System.out.println(superClassTuple.getLeft().toString());
+//        System.out.println(superClassTuple.getLeft().toString());
 
         final ImprovedTuple<Long, String> improvedTuple = new ImprovedTuple<>(9L, "Bob Bobbovitch");
-        System.out.println(improvedTuple.process(9L));
-        System.out.println(improvedTuple.process(0L));
-        System.out.println(improvedTuple.process(19L));
+//        System.out.println(improvedTuple.process(9L));
+//        System.out.println(improvedTuple.process(0L));
+//        System.out.println(improvedTuple.process(19L));
+
+        /*
+        Now I want to inherit my tuple to become triple and I want to keep the universal approach
+        lets test this with Triple filled with my SuperClass as middle variable
+         */
+        Triple<Integer, SuperClass, String> triple = new Triple<>(35, new SuperClass(), "randonstring");
+        Triple<Double, Long, Long> differentTriple = new Triple<>(0.35, 0L, System.currentTimeMillis());
+        System.out.println(triple.getBody().toString()); // Should be overriden toString from SuperClass!
+        System.out.println(differentTriple.getIndex() + "---" + differentTriple.getBody() + "---" + differentTriple.process(0.35));
 
 
     }
---
+++ src/main/java/hexlet/Triple.java
@@ -0,0 +1,19 @@
+package hexlet;
+
+/**
+ * Created by Iidwuurliik on 3/10/2017.
+ * This class will provide me with generic Triple
+ * based on my Improved Tuple
+ */
+public class Triple<Input, Middle, Return> extends ImprovedTuple<Input, Return> {
+    private final Middle body;
+
+    public Triple(Input index, Middle body, Return content) {
+        super(index, content);
+        this.body = body;
+    }
+
+    public Middle getBody() {
+        return body;
+    }
+}
