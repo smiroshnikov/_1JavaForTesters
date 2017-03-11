bd1f2ad6229b85ff1c138fbf6b8ff79e12dd46fa
smiroshnikov <zer00aeon@gmail.com>
2017-03-11 14:45:09 +0200

    Chapter 19 - first Spy :)

---
+++ src/main/java/hexlet/XO/Player.java
@@ -0,0 +1,9 @@
+package hexlet.XO;
+
+/**
+ * Player representation for XO
+ */
+public class Player {
+    String name;
+    String figure;
+}
---
+++ src/main/java/hexlet/XO/Point.java
@@ -0,0 +1,10 @@
+package hexlet.XO;
+
+/**
+ * Hexlet XO
+ * Point represents location on the board
+ */
+public class Point {
+    int x;
+    int y;
+}
