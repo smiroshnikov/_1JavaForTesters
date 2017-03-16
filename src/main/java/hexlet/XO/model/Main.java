cc81aedb460c6144ca16deaddfb82c6e60519529
smiroshnikov <sergeim.qa@gmail.com>
2017-03-16 19:14:47 +0200

    Hexlet , sync with Game design

--- src/main/java/hexlet/XO/model/Main.java
+++ src/main/java/hexlet/XO/model/Main.java
@@ -2,23 +2,28 @@ package hexlet.XO.model;
 
 /**
  * Created by miross1 on 3/16/2017.
+ * Inheritance , HW
  */
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         TwoPlayersGame twoP = new TwoPlayersGame("Bob", "Mike");
         ThreePlayersGHame threeP = new ThreePlayersGHame("Kate", "Stella", "Anna");
+        Game g = new ThreePlayersGHame("b", "c", "l");
+
 
         printGameNameAndBoard(threeP);
         printGameNameAndBoard(twoP);
         printGameNameAndBoard(game);
+        printGameNameAndBoard(null); // why can this accept null ?! Max
+
     }
 
     public static void printGameNameAndBoard(final Game game) {
-        // this class can accept instance of Game class as argument
+        // this class can accept an instance of Game class as argument
         // or any other class that extends game
         // I was not aware of such usage when I created this class
-        // tand that is fucking cool
-        game.printGameName();
+        // that that is fucking cool!
+        Game.printGameName();
     }
 }
