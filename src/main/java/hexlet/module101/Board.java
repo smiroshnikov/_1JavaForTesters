ad329540ec174134e0b7317cc3a35bf724f54c45
smiroshnikov <sergeim.qa@gmail.com>
2017-03-17 17:32:15 +0200

    XO - writing horrible code in order to refactor and never write like that again !

--- src/main/java/hexlet/module101/Board.java
+++ src/main/java/hexlet/module101/Board.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 
 public class Board {
 
-    private static final int SIZE_FIELD = 9;
+    private static final int SIZE_FIELD = 3;
 
-    private Figure[] figures = new Figure[SIZE_FIELD];
+    private Figure[][] figures = new Figure[SIZE_FIELD][SIZE_FIELD];
 
     public void showBoard() {
-        System.out.println(Arrays.toString(this.figures)); // Arrays , handy
+        //System.out.println(Arrays.toString(this.figures)); // Arrays , handy
+        System.out.println(Arrays.deepToString(this.figures)); // handy works with [][][][].....
 
     }
 
     public void initArray() {
-        figures[0] = new Figure("X");
-        figures[3] = new Figure("O");
+
+        figures[0][0] = new Figure("O");
+        figures[0][2] = new Figure("X");
     }
 
 }
