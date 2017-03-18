499161a97144b4221fe3aefc0f6d6ef2de863ea8
smiroshnikov <sergeim.qa@gmail.com>
2017-03-18 23:06:55 +0200

    XO  , keep switching

--- src/main/java/hexlet/module101/MenuView.java
+++ src/main/java/hexlet/module101/MenuView.java
@@ -4,13 +4,48 @@ import java.util.Scanner;
 
 /**
  * Created by Janna on 18.03.2017.
+ * Working from my mom's comp
  */
+
 public class MenuView {
 
-    public static int choice;
-    final Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
 
+    private static final int START_CODE = 1;
+
+    public static void main(String[] args) {
+        System.out.println("1-Start");
+        System.out.println("2-Load");
+        System.out.println("3-Settings");
+        System.out.println("4-Exit");
+        System.out.print("> ");
+
+        final Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            // break is omited on purpose so get conclusions ....
+            case 1: {
+                System.out.println("Unable to start FATAL_ERROR");
+            }
+            case 2: {
+                System.out.println("Loading///");
+            }
+            case 3: {
+                System.out.println("Settings are diasbled ");
+                break;
+            }
+            case 4: {
+                System.out.println("Exiting....");
+                break;
+
+            }
+            default: {
+                System.out.println("Invalid choice");
+                break;
+            }
+
+
+        }
 
 
     }
