208b75ec304c11fd5d4c557eaa66f7f46001758a
smiroshnikov <zer00aeon@gmail.com>
2017-03-18 18:23:01 +0200

    XO - player position validation, solving homework

---
+++ src/main/java/hexlet/module101/MenuView.java
@@ -0,0 +1,18 @@
+package hexlet.module101;
+
+import java.util.Scanner;
+
+/**
+ * Created by Janna on 18.03.2017.
+ */
+public class MenuView {
+
+    public static int choice;
+    final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
