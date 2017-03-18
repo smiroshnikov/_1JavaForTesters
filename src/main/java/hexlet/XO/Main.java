dc921f3435ace199a3a52384a7c2de34bed8c1a4
smiroshnikov <zer00aeon@gmail.com>
2017-03-18 16:15:21 +0200

    XO - player position validation, solving homework

--- src/main/java/hexlet/XO/Main.java
+++ src/main/java/hexlet/XO/Main.java
@@ -4,11 +4,14 @@ import hexlet.XO.controllers.CurrentMoveController;
 import hexlet.XO.model.Field;
 import hexlet.XO.model.Point;
 
+import java.util.Arrays;
+
 /**
  * Tic tac toe main
  */
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String... args) {
         final Field field = new Field();
         final CurrentMoveController cmc = new CurrentMoveController();
 
@@ -16,6 +19,24 @@ public class Main {
         p.x = 1;
         p.y = 1;
 
+        if (!cmc.currentMove(field).equals("X")) {
+            throw new RuntimeException("does not return X when the field is empty");
+        }
+
+        for (int i = 0; i < field.getSize(); i++)
+            for (int i2 = 0; i2 < field.getSize(); i2++) {
+                p.x = i;
+                p.y = i2;
+                final String figure = (i * 3 + i2) % 2 == 0 ? "X" : "O";
+                field.setFigure(p, figure);
+                System.out.printf(" putting figure: %s to the: X:%d Y:%d\n", figure, p.x, p.y);
+                if (cmc.currentMove(field).equals(figure)) {
+                   throw new RuntimeException(String.format("returns %s for the field: %s", figure, Arrays.deepToString(field.figures)));
+                }
+
+            }
 
     }
+
+
 }
--- src/main/java/hexlet/XO/controllers/CurrentMoveController.java
+++ src/main/java/hexlet/XO/controllers/CurrentMoveController.java
@@ -1,6 +1,7 @@
 package hexlet.XO.controllers;
 
 import hexlet.XO.model.Field;
+import hexlet.XO.model.Point;
 
 /**
  * Created by Iidwuurliik on 3/13/2017.
@@ -11,7 +12,34 @@ public class CurrentMoveController {
 
     public String currentMove(Field field) {
 
-        return "X";
+        Point p = new Point();
+        int xCounter = 0;
+        int oCounter = 0;
+
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int j = 0; j < field.getSize(); j++) {
+                p.x = i;
+                p.y = j;
+
+                if (field.getFigure(p) != null) {
+                    if (field.getFigure(p).equals("X")) {
+                        xCounter += 1;
+                        System.out.println("X = " + xCounter);
+                    }
+
+                    if (field.getFigure(p).equals("O")) {
+                        oCounter += 1;
+                        System.out.println("O = " + oCounter);
+                    }
+                }
+            }
+        }
+
+        if ((xCounter == 0 && oCounter == 0) || (xCounter == oCounter)) {
+            return "X";
+        }
+
+        return "O";
 
     }
 }
