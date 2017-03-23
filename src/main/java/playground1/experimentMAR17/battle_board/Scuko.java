9c8eb6e16313a8baee0ec72e53c20c0487aeda26
smiroshnikov <sergeim.qa@gmail.com>
2017-03-23 17:27:50 +0200

    Hexlet , homework ,today I am ending this annoying HW I have been so lazy about . !

--- src/main/java/playground1/experimentMAR17/battle_board/Scuko.java
+++ src/main/java/playground1/experimentMAR17/battle_board/Scuko.java
@@ -2,6 +2,7 @@ package playground1.experimentMAR17.battle_board;
 
 /**
  * Created by miross1 on 3/23/2017.
+ * HATE Tic tac toe !
  */
 public class Scuko {
     public static void main(String[] args) {
@@ -9,26 +10,52 @@ public class Scuko {
             System.out.println("");
 
             for (int j = 0; j < 3; j++) {
-                if (i == 0) {
-                    System.out.print("*");
-                } else if (i == 1) {
-                    System.out.print("#");
-                } else if (i == 2) {
-                    System.out.print("@");
+
+                //region Horizontal check
+//                switch (i) {
+//                    case 0: {
+//                        System.out.print("*");
+//                        break;
+//                    }
+//                    case 1: {
+//                        System.out.print("#");
+//                        break;
+//                    }
+//                    case 2: {
+//                        System.out.print("@");
+//                        break;
+//                    }
+//                }
+                //endregion
+
+                //region Vertical check
+//                switch (j) {
+//                    case 0: {
+//                        System.out.print("\t%\t");
+//                        break;
+//                    }
+//                    case 1: {
+//                        System.out.print("\t^\t");
+//                        break;
+//                    }
+//                    case 2: {
+//                        System.out.print("\t&\t");
+//                        break;
+//                    }
+//                }
+                //endregion
+
+                if (i == j) {
+                    // normal diagonal
+                    System.out.print("\t*");
+                } else if ((2 - i) == j) {
+                    // reverted diagonal
+                    System.out.print("\t#");
+                } else {
+                    //NON DIAGONAL CHARACTERS!
+                    System.out.print("\t" + i);
                 }
-
-
-//                if (i == j) {
-//                    // normal diagonal
-//                    System.out.print("\t*");
-//                } else if ((2 - i) == j) {
-//                    // reverted diagonal
-//                    System.out.print("\t#");
-//                } else {
-                // everything else
-//                System.out.print("\t" + i);
             }
-
         }
     }
 }
